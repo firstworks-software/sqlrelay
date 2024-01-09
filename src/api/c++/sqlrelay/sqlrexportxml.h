26a2588ed9d83df8170aad3910a06656780f2934
David Muse <david.muse@firstworks.com>
2024-01-09 02:26:25 -0500

    comments

--- src/api/c++/sqlrelay/sqlrexportxml.h
+++ src/api/c++/sqlrelay/sqlrexportxml.h
@@ -20,7 +20,50 @@ class SQLRCLIENT_DLLSPEC sqlrexportxml : public sqlrexport {
 		 *  "filename" in XML format.
 		 *
 		 *  If "table" is non-null then the table name is embedded in
-		 *  the XML file in the <table/> tag.
+		 *  the XML file in the <table/> tag.  If "table" is null then
+		 *  the <table> tag is omitted.
+		 *
+		 *  The XML format is as follows:
+		 *  (This example is a table of US states, exported from
+		 *  PostgreSQL)
+		 *
+		 *  <?xml version="1.0"?>
+		 *  <table name="states">
+		 *  <columns count="6">
+		 *          <column name="state_id" type="int4"/>
+		 *          <column name="state_name" type="varchar"/>
+		 *          <column name="state_abbreviation" type="varchar"/>
+		 *          <column name="latitude" type="numeric"/>
+		 *          <column name="longitude" type="numeric"/>
+		 *          <column name="creation_date" type="date"/>
+		 *  </columns>
+		 *  <rows>
+		 *          <row>
+		 *          <field>1</field>
+		 *          <field>Louisiana</field>
+		 *          <field>LA</field>
+		 *          <field>31.259800</field>
+		 *          <field>-92.658700</field>
+		 *          <field>2005-08-16</field>
+		 *          </row>
+		 *          <row>
+		 *          <field>2</field>
+		 *          <field>Texas</field>
+		 *          <field>TX</field>
+		 *          <field>31.653400</field>
+		 *          <field>-100.546900</field>
+		 *          <field>2005-08-17</field>
+		 *          </row>
+		 *          <row>
+		 *          <field>3</field>
+		 *          <field>Georgia</field>
+		 *          <field>GA</field>
+		 *          <field>32.750300</field>
+		 *          <field>-83.737800</field>
+		 *          <field>2005-08-16</field>
+		 *          </row>
+		 *  </rows>
+		 *  </table>
 		 *
 		 *  Returns true on success and false if an error occurred. */
 		bool	exportToFile(const char *filename, const char *table);
--- src/api/c++/sqlrelay/sqlrimportxml.h
+++ src/api/c++/sqlrelay/sqlrimportxml.h
@@ -7,61 +7,7 @@
 #include <sqlrelay/private/sqlrimportxmlincludes.h>
 
 /** The sqlrimportxml class implements sqlrimport for XML files, specifically
- *  XML files exported by the sqlrexportxml class.
- *
- *  The format of XML files exported by sqlrexportxml and imported by
- *  sqlrimportxml is as follows.
- *
- *  For a table:
- *  (In this example, a table of US states, exported from PostgreSQL)
- *
- *  <?xml version="1.0"?>
- *  <table name="states">
- *  <columns count="6">
- *          <column name="state_id" type="int4"/>
- *          <column name="state_name" type="varchar"/>
- *          <column name="state_abbreviation" type="varchar"/>
- *          <column name="latitude" type="numeric"/>
- *          <column name="longitude" type="numeric"/>
- *          <column name="creation_date" type="date"/>
- *  </columns>
- *  <rows>
- *          <row>
- *          <field>1</field>
- *          <field>Louisiana</field>
- *          <field>LA</field>
- *          <field>31.259800</field>
- *          <field>-92.658700</field>
- *          <field>2005-08-16</field>
- *          </row>
- *          <row>
- *          <field>2</field>
- *          <field>Texas</field>
- *          <field>TX</field>
- *          <field>31.653400</field>
- *          <field>-100.546900</field>
- *          <field>2005-08-17</field>
- *          </row>
- *          <row>
- *          <field>3</field>
- *          <field>Georgia</field>
- *          <field>GA</field>
- *          <field>32.750300</field>
- *          <field>-83.737800</field>
- *          <field>2005-08-16</field>
- *          </row>
- *  </rows>
- *  </table>
- *
- *
- *  For a sequence:
- *  (In this example, a sequence used to generate ids for the table above,
- *  exported from PostgreSQL)
- *
- *  <?xml version="1.0"?>
- *  <sequence name="state_ids" value="4"/>
- *
- */
+ *  XML files exported by the sqlrexportxml class. */
 class SQLRCLIENT_DLLSPEC sqlrimportxml : public sqlrimport, public xmlsax {
 	public:
 		/** Creates an instance of the sqlrimportxml class. */
@@ -70,11 +16,64 @@ class SQLRCLIENT_DLLSPEC sqlrimportxml : public sqlrimport, public xmlsax {
 		/** Destroys this instance of the sqlrimportxml class. */
 		~sqlrimportxml();
 
-		/** Imports data from "filename".  The table (or sequence)
-		 *  to import the data into will be deried from the name
-		 *  attribute of the table or sequence tag inside the file and
-		 *  may be overridden using setObjectName().  Returns true on
-		 *  succes and false if an error occurred. */
+		/** Imports data from "filename".  The table or sequence to
+		 *  import the data into will be deried from the name attribute
+		 *  of the table or sequence tag inside the file and may be
+		 *  overridden using setObjectName().
+		 *
+		 *  The XML format is as follows.
+		 *
+		 *  For a table:
+		 *  (This example is a table of US states, exported from
+		 *  PostgreSQL)
+		 *
+		 *  <?xml version="1.0"?>
+		 *  <table name="states">
+		 *  <columns count="6">
+		 *          <column name="state_id" type="int4"/>
+		 *          <column name="state_name" type="varchar"/>
+		 *          <column name="state_abbreviation" type="varchar"/>
+		 *          <column name="latitude" type="numeric"/>
+		 *          <column name="longitude" type="numeric"/>
+		 *          <column name="creation_date" type="date"/>
+		 *  </columns>
+		 *  <rows>
+		 *          <row>
+		 *          <field>1</field>
+		 *          <field>Louisiana</field>
+		 *          <field>LA</field>
+		 *          <field>31.259800</field>
+		 *          <field>-92.658700</field>
+		 *          <field>2005-08-16</field>
+		 *          </row>
+		 *          <row>
+		 *          <field>2</field>
+		 *          <field>Texas</field>
+		 *          <field>TX</field>
+		 *          <field>31.653400</field>
+		 *          <field>-100.546900</field>
+		 *          <field>2005-08-17</field>
+		 *          </row>
+		 *          <row>
+		 *          <field>3</field>
+		 *          <field>Georgia</field>
+		 *          <field>GA</field>
+		 *          <field>32.750300</field>
+		 *          <field>-83.737800</field>
+		 *          <field>2005-08-16</field>
+		 *          </row>
+		 *  </rows>
+		 *  </table>
+		 *
+		 *
+		 *  For a sequence:
+		 *  (This example is a sequence used to generate ids for the
+		 *  table above, exported from PostgreSQL)
+		 *
+		 *  <?xml version="1.0"?>
+		 *  <sequence name="state_ids" value="4"/>
+		 *
+		 *  Returns true on success and false if an error occurred. */
 		bool	importFromFile(const char *filename);
 
 	#include <sqlrelay/private/sqlrimportxml.h>
