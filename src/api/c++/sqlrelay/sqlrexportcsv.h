c997918cd14524e80df0e91f199e6acc09b0f5f7
David Muse <david.muse@firstworks.com>
2024-01-23 01:55:15 -0500

    updates

--- src/api/c++/sqlrelay/sqlrexportcsv.h
+++ src/api/c++/sqlrelay/sqlrexportcsv.h
@@ -27,7 +27,56 @@ class SQLRCLIENT_DLLSPEC sqlrexportcsv : public sqlrexport {
 
 		/** Exports the result set of the cursor currently in use as
 		 *  set by the most recent call to setSqlrCursor() to the
-		 *  JSON domnode "jsondomnode".
+		 *  JSON domnode "jsondomnode" in a format the represents the
+		 *  CSV using JSON.
+		 *
+		 *  The following result set:
+		 *
+		 *  col1,col2,col3,col4
+		 *  =====================
+		 *  0,0.0,field00,field01
+		 *  1,1.1,field10,field11
+		 *
+		 *  would be represented like:
+		 *
+		 *  <h t="a">
+		 *    <v t="s" v="col1"/>
+		 *    <v t="s" v="col2"/>
+		 *    <v t="s" v="col3"/>
+		 *    <v t="s" v="col4"/>
+		 *  </h>
+		 *  <r t="a">
+		 *    <v t="n" v="0"/>
+		 *    <v t="n" v="0.0"/>
+		 *    <v t="s" v="field00"/>
+		 *    <v t="s" v="field01"/>
+		 *  </r>
+		 *  <r t="a">
+		 *    <v t="n" v="1"/>
+		 *    <v t="n" v="1.1"/>
+		 *    <v t="s" v="field10"/>
+		 *    <v t="s" v="field11"/>
+		 *  </r>
+		 *
+		 *  This format is similar to the DOM tree that the rudiments
+		 *  class csvdom defines to represent a CSV, though it uses the
+		 *  rules defined for representing JSON by the rudiments class
+		 *  jsondom.
+		 *
+		 *  It consists of a "h"(eader) array, each "v"(alue) of which
+		 *  contains a "t"(ype) attribute of either "s"(tring) or
+		 *  "n"(umber) and a "v"(alue) attribute containing the column
+		 *  name.
+		 *
+		 *  It also consists of "r"(ecord) arrays, each "v"(alue)
+		 *  of which also contains a "t"(ype) attribute of either
+		 *  "s"(tring) or "n"(umber) and a "v"(alue).
+		 *
+		 *  The "h"(eader) and "r"(ecord) nodes are inspired by the
+		 *  CSV representation defined by csvdom, but the nodes of the
+		 *  tree are all consistent with the JSON representation
+		 *  defined by jsondom.  As such, the tree can be operated on
+		 *  by methods that can operate on a jsondom tree.
 		 *
 		 *  The "table" argument is ignored in this implementation.
 		 *
