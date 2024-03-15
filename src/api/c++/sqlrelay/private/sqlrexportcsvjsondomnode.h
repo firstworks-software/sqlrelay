27d9673793d3d2bb7319871be1446c85abe584a9
David Muse <david.muse@firstworks.com>
2024-03-15 18:44:18 -0400

    new file

---
+++ src/api/c++/sqlrelay/private/sqlrexportcsvjsondomnode.h
@@ -0,0 +1,8 @@
+// Copyright (c) 1999-2018 David Muse
+// See the file COPYING for more information
+
+	private:
+		bool	startProcessingColumns();
+		bool	exportColumnName(bool first);
+		bool	startProcessingRow();
+		bool	exportField(bool first);
