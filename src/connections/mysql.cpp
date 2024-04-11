4941c8b1f7c54865c8a48c69d1392e087dc6db78
David Muse <david.muse@firstworks.com>
2024-04-11 00:14:36 -0400

    added catalog/schema clauses

--- src/connections/mysql.cpp
+++ src/connections/mysql.cpp
@@ -729,6 +729,12 @@ const char *mysqlconnection::getTableListQuery(bool wild,
 	tablelistquery.append("from ");
 	tablelistquery.append("	information_schema.tables ");
 	tablelistquery.append("where ");
+	tablelistquery.append(" table_catalog='def' ");
+	tablelistquery.append(" and ");
+	tablelistquery.append(" table_schema='");
+	tablelistquery.append(getCurrentDatabase());
+	tablelistquery.append("' ");
+	tablelistquery.append(" and ");
 	if (wild) {
 		tablelistquery.append("	table_name like '%s' ");
 		tablelistquery.append("	and ");
