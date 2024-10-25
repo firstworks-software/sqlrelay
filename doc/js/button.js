3a208abe148b4ab4c2d95f8abd99c89aae328a95
David Muse <david.muse@firstworks.com>
2024-10-25 16:08:34 -0400

    js updates

--- doc/js/button.js
+++ doc/js/button.js
@@ -2,32 +2,26 @@ function endsWith(str,ending) {
 	return str.indexOf(ending,str.length-ending.length)!==-1;
 }
 
+function contains(str,part) {
+	return str.includes(part);
+}
+
 function setActive(btn,img) {
-	document.getElementById("button-"+btn).style.backgroundImage="url(images/"+img+")";
+	document.getElementById("button-"+btn).style.backgroundImage="url("+img+")";
 }
 
 function setButton() {
-	if (endsWith(document.location.pathname,"technologies.html")) {
-		setActive("technologies","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"services.html")) {
-		setActive("services","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"examples.html")) {
-		setActive("examples","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"index.html")) {
-		setActive("about","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"download.html")) {
-		setActive("download","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"license.html")) {
-		setActive("license","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"faststart.html")) {
-		setActive("faststart","lightbluedenim.png");
+	if (endsWith(document.location.pathname,"index.html")) {
+		setActive("about","images/lightbluedenim.png");
 	} else if (endsWith(document.location.pathname,"documentation.html")) {
-		setActive("documentation","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"nativeprotocol.html")) {
-		setActive("documentation","lightbluedenim.png");
+		setActive("documentation","images/lightbluedenim.png");
+	} else if (endsWith(document.location.pathname,"documentation.html") || contains(document.location.pathname,"/admin/") || contains(document.location.pathname,"/api/") || contains(document.location.pathname,"/features/") || contains(document.location.pathname,"/howtos/") || contains(document.location.pathname,"/programming/")) {
+		setActive("documentation","../images/lightbluedenim.png");
+	} else if (endsWith(document.location.pathname,"download.html")) {
+		setActive("download","images/lightbluedenim.png");
+	} else if (endsWith(document.location.pathname,"license.html")) {
+		setActive("license","images/lightbluedenim.png");
 	} else if (endsWith(document.location.pathname,"support.html")) {
-		setActive("support","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"successstories.html")) {
-		setActive("successstories","lightbluedenim.png");
+		setActive("support","images/lightbluedenim.png");
 	}
 }
