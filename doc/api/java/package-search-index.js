d85b0a8476f3c52a4009971cc79621d35ee50639
David Muse <david.muse@firstworks.com>
2024-12-03 02:43:01 -0500

    regenerated docs

--- doc/api/java/package-search-index.js
+++ doc/api/java/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.firstworks.sqlrelay"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.firstworks.sqlrelay"}];updateSearchResults();
